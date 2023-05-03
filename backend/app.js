b371e2f671c58056a775571b9cc8d99acf859b27
Shelo2001 <sheliaakaki47@gmail.com>
2023-05-03 17:09:00 +0400

    some changes

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,13 @@ const app = express()
 const serverless = require('serverless-http')
 const usersRoutes = require('./routes/usersRoutes')
 const router = require('./routes/usersRoutes')
+const {
+  getUsers,
+  addUser,
+  getUserById,
+  updateUserById,
+  deleteUser,
+} = require('./controllers/usersController')
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/api/users', usersRoutes)
@@ -13,6 +20,15 @@ app.listen(port, () => {
   console.log(`Server started on port ${port}`)
 })
 
+router.get('/', getUsers)
+router.post('/create', addUser)
+router
+  .route('/user/:id')
+  .get(getUserById)
+  .put(updateUserById)
+  .delete(deleteUser)
+
 app.use('/.netlify/functions/app', router)
 
+module.exports = app
 module.exports.handler = serverless(app)
